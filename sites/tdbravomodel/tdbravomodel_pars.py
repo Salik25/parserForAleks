683d5c279dabcb3ac337ad6ea68fc08d34d8230c
Ivan <Salik52@yandex.ru>
2022-05-05 12:31:58 +0500

    ttdbravo

--- sites/tdbravomodel/tdbravomodel_pars.py
+++ sites/tdbravomodel/tdbravomodel_pars.py
@@ -18,7 +18,6 @@ with open("index.html", "r", encoding="utf-8") as f:
     for i in cat_links:
         for j in i.find_all('a'):
             catalog_links[url_home + j['href']] = j.text
-    print(catalog_links)
 
 cat_prod_all = {}
 for i in catalog_links:
@@ -48,27 +47,29 @@ for i in catalog_links:
                 for j in i.find_all('div', class_='swiper-slide five-slide-first vis-slide-five base-element'):
                     cat_prod_all[url_home + j.find('a')[
                         'href']] = j.find('span', class_='hits-prod-name-txt').text
-            print(cat_prod_all, len(cat_prod_all))
-#
-# final = {'Название позиции': [], 'Картинка': [], 'Описание': [], 'Характеристики': []}
-# count = 0
-# for i in cat_prod_all:
-#     r = requests.get(i)
-#     print(i, count)
-#     with open("index.html", "w+", encoding="utf-8") as f:
-#         f.write(r.text)
-#     with open("index.html", "r", encoding="utf-8") as f:
-#         contents = f.read()
-#         soup = BeautifulSoup(contents, 'lxml')
-#         content = soup.find('div', class_='container main-container')
-#         final['Название позиции'].append(content.find('h1', class_='main-heading').text)
-#         final['Картинка'].append(
-#             url + content.find('div', class_='bx_bigimages_aligner image-big-container').find('a')['href'])
-#         final['Описание'].append(content.find('div', class_='bx_item_description main-heading-wrapper').text)
-#         final['Характеристики'].append(content.find('div', class_='item_info_section').find('dl').text)
-#
-#         # print(final)
-#     count += 1
-# df = DataFrame.from_dict(final, orient='index')
-# df = df.transpose()
-# df.to_excel('tdbravomodel.xlsx')
+
+final = {'Название позиции': [], 'Картинка': [], 'Характеристики': []}
+count = 0
+for i in cat_prod_all:
+    r = requests.get(i)
+    print(i, count)
+    with open("index.html", "w+", encoding="utf-8") as f:
+        f.write(r.text)
+    with open("index.html", "r", encoding="utf-8") as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+        content = soup.find('div', class_='content-page prod-item-page prod-min-title')
+        final['Название позиции'].append(content.find('h1', class_='h-cat-name-txt grad-txt').text)
+        # print(content.find('div', class_='pl-col-right-mob'))
+        try:
+            final['Картинка'].append(
+                url_home + content.find('div', class_='pl-col-right-mob').find('img')['data-src'])
+        except AttributeError:
+            final['Картинка'].append('-')
+        final['Характеристики'].append(content.find('div', id='PROPERTIES').find('div', class_='prod-big-col prod-char-cont p8-959').text)
+
+        # print(final)
+    count += 1
+df = DataFrame.from_dict(final, orient='index')
+df = df.transpose()
+df.to_excel('tdbravomodel.xlsx')
